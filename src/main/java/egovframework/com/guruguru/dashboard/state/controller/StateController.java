e24ad07db8391fd599e25e1e69a2fbd9ba06345a
jisoo <auready21@gmail.com>
2013-10-12 10:59:02 +0900

    state service sample

---
+++ src/main/java/egovframework/com/guruguru/dashboard/state/controller/StateController.java
@@ -0,0 +1,37 @@
+package egovframework.com.guruguru.dashboard.state.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/state")
+public class StateController {
+
+	@RequestMapping("/getStateInfo")
+	@ResponseBody
+	public String getStateInfo() {
+		System.out.println(System.getProperty("os.name"));
+		
+		String line = "";
+		
+		try {
+			ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", "vmstat");
+			Process proc = pb.start();
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+			
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException ioe) {
+			throw new RuntimeException(ioe.getMessage());
+		}
+		
+		return "sample";
+	}
+}
