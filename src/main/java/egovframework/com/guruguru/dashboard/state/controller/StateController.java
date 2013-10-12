7cb5c30389cf1d87674cdd3057edca4c76a4eec8
jisoo <auready21@gmail.com>
2013-10-12 14:17:05 +0900

    memory service add

--- src/main/java/egovframework/com/guruguru/dashboard/state/controller/StateController.java
+++ src/main/java/egovframework/com/guruguru/dashboard/state/controller/StateController.java
@@ -3,6 +3,7 @@ package egovframework.com.guruguru.dashboard.state.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -18,11 +19,13 @@ public class StateController {
 
 	@RequestMapping("/getStateInfo")
 	@ResponseBody
-	public String getStateInfo() {
+	public Map<String, Object> getStateInfo() {
 		String os = System.getProperty("os.name");
 		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
 		if (os.equalsIgnoreCase("linux")) {
-			getMemroyInfo(null);
+			getMemroyInfo(map);
 		}
 		
 //		System.out.println(System.getProperty("os.name"));
@@ -49,7 +52,7 @@ public class StateController {
 //			throw new RuntimeException(ioe.getMessage());
 //		}
 		
-		return "sample";
+		return map;
 	}
 	
 	private void getMemroyInfo(Map<String, Object> map) {
@@ -57,13 +60,18 @@ public class StateController {
 		
 		int i = 0;
 		
+		String[] memInfo = {"memTotal", "memUsed", "memFree", "memShared", "memBuffers"};
 		Iterator<String> it = Splitter.on(' ').omitEmptyStrings().split(memoryInfo).iterator();
 		
 		while (it.hasNext()) {
-			System.out.println(it.next());
+			if (i > 10) break;
+			
+			if (i > 5) {
+				map.put(memInfo[i - 6], it.next());
+			}
+			
+			i++;
 		}
-		
-		
 	}
 	
 	private String executeProcess(String... cmd) {
